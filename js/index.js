693daa01784da5fd94129f8d56b03b477dc68a01
eunhye han <hellou8363@gmail.com>
2023-01-31 11:56:14 +0900

    메인페이지(index.html) 수정

--- js/index.js
+++ js/index.js
@@ -1,34 +1,10 @@
-const contentText = document.querySelector("#contentText p");
-const text = contentText.innerText.split("\n");
-const btnMore = document.getElementById("btnMore");
-const btnHidden = document.getElementById("btnHidden");
-const favorite = document.getElementById('favorite');
-const noFavorite = document.getElementById('noFavorite');
-
-contentText.innerHTML = text[0];
-
-// 더보기 이벤트
-btnMore.addEventListener("click", () => {
-  contentText.innerText = text;
-  btnMore.style.display = "none";
-  btnHidden.style.display = "block";
+// Menu Toggle Event
+const menu = document.querySelector(".hamburger-icon");
+menu.addEventListener("click", () => {
+  if (menu.classList.toggle("toggle")) {
+    document.getElementById('menu').style.display = "block";
+  } else {
+    document.getElementById('menu').style.display = "none";
+  }
 });
 
-// 숨기기 이벤트
-btnHidden.addEventListener("click", () => {
-  contentText.innerHTML = text[0];
-  btnHidden.style.display = "none";
-  btnMore.style.display = "block";
-});
-
-// 좋아요 이벤트
-favorite.addEventListener('click', () => {
-  noFavorite.style.display = 'inline';
-  favorite.style.display = 'none';
-});
-
-// 좋아요 취소 이벤트
-noFavorite.addEventListener('click', () => {
-  favorite.style.display = 'inline';
-  noFavorite.style.display = 'none';
-});

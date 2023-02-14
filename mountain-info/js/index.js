cd21a7caa55c4b34b737944542c96ee64c5d950c
eunhye han <hellou8363@gmail.com>
2023-02-15 05:51:49 +0900

    메인페이지(산/모집/후기) 목록 범위에서 휠 스크롤 시 가로 이동 구현

--- mountain-info/js/index.js
+++ mountain-info/js/index.js
@@ -8,6 +8,10 @@ addEventListener("click", (e) => {
     $("#userImg").classList.remove("toggle");
     $(".menubar").style.display = "none";
   } // if
+  if (e.target.className.indexOf("king") < 0) {
+    // console.log(e.target.className.indexOf("king"));
+    $(".zoom-rankers-top3").style.display = "none";
+  }
 });
 
 // 메인페이지 - 회원 이미지 클릭 시 메뉴바 표시
@@ -20,24 +24,32 @@ $("#userImg").addEventListener("click", () => {
 });
 
 // 메인페이지 - Rank에서 특정 왕 hover시 1 ~ 3위까지 보여주는 랭크목록 표시
+$(".deep-king").addEventListener("mouseover", () => {
+  $(".zoom-rankers-top3").style.display = "block";
+});
+$(".high-king").addEventListener("mouseover", () => {
+  $(".zoom-rankers-top3").style.display = "block";
+});
+$(".true-king").addEventListener("mouseover", () => {
+  $(".zoom-rankers-top3").style.display = "block";
+});
+$(".review-king").addEventListener("mouseover", () => {
+  $(".zoom-rankers-top3").style.display = "block";
+});
 
+// 메인페이지 - 산정보, 모집, 후기 글 목록 위에서
+// 휠 스크롤 시 가로 슬라이드 구현
+$(".mountain-info .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY * 2;
+});
 
-// 메인페이지의 산정보, 모집, 후기 글에 대한 가로 슬라이드 구현
-// 조건: 타겟(.wrap)위엣 마우스를 스크롤 할 때
-// 강제 shift를 적용해서 휠로 좌/우 이동이 되게 할 것
-// 2023-02-14: 마우스 휠로 가로 스크롤을 움직이게 하는 방법은
-//             세로 스크롤까지 같이 움직여서 좌/우버튼이나 드래그 방식을 생각해야 할듯
-// $(".wrap").addEventListener("wheel", (e) => {
-//   // document.body.style.overflow = "hidden";
-//   // console.log(window.scrollTop);
-//   e.currentTarget.scrollBy(e.deltaY * 3, 0);
-//   console.log(e.currentTarget);
-// });
-// $(".wrap").addEventListener("mouseout", (e) => {
-//   console.log('mouseout')
-// });
+$(".recruit .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY;
+});
 
-
-
-
-
+$(".review .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY;
+});

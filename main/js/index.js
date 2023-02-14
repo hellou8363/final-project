9d9629c1efb80f7edc679d0f19748bf7391dc7e6
eunhye han <hellou8363@gmail.com>
2023-02-15 08:46:36 +0900

    검색 바 버튼 위치 수정

--- main/js/index.js
+++ main/js/index.js
@@ -2,12 +2,13 @@ const $ = (selector) => document.querySelector(selector);
 
 // Event init
 addEventListener("click", (e) => {
-  // console.log(e.target.id);
   // 메인페이지 - 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
   if (e.target.id !== "userImg") {
     $("#userImg").classList.remove("toggle");
     $(".menubar").style.display = "none";
   } // if
+
+  // temp code - Rank의 하위 div를 벗어날 시 hover로 활성화된 박스 제거
   if (e.target.className.indexOf("king") < 0) {
     // console.log(e.target.className.indexOf("king"));
     $(".zoom-rankers-top3").style.display = "none";
@@ -23,19 +24,24 @@ $("#userImg").addEventListener("click", () => {
   }
 });
 
+
 // 메인페이지 - Rank에서 특정 왕 hover시 1 ~ 3위까지 보여주는 랭크목록 표시
 $(".deep-king").addEventListener("mouseover", () => {
   $(".zoom-rankers-top3").style.display = "block";
 });
+
 $(".high-king").addEventListener("mouseover", () => {
   $(".zoom-rankers-top3").style.display = "block";
 });
+
 $(".true-king").addEventListener("mouseover", () => {
   $(".zoom-rankers-top3").style.display = "block";
 });
+
 $(".review-king").addEventListener("mouseover", () => {
   $(".zoom-rankers-top3").style.display = "block";
 });
+
 
 // 메인페이지 - 산정보, 모집, 후기 글 목록 위에서
 // 휠 스크롤 시 가로 슬라이드 구현
@@ -46,10 +52,10 @@ $(".mountain-info .wrap").addEventListener("wheel", (e) => {
 
 $(".recruit .wrap").addEventListener("wheel", (e) => {
   e.preventDefault();
-  e.currentTarget.scrollLeft += e.deltaY;
+  e.currentTarget.scrollLeft += e.deltaY * 2;
 });
 
 $(".review .wrap").addEventListener("wheel", (e) => {
   e.preventDefault();
-  e.currentTarget.scrollLeft += e.deltaY;
+  e.currentTarget.scrollLeft += e.deltaY * 2;
 });

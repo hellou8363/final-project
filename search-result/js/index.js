7014fbca24e2ce4894eaadb91171c112cbffaf9b
eunhye han <hellou8363@gmail.com>
2023-02-15 21:24:52 +0900

    헤더 스타일시트 body background image 추가

---
+++ search-result/js/index.js
@@ -0,0 +1,33 @@
+const $ = (selector) => document.querySelector(selector);
+
+// Event init
+addEventListener("click", (e) => {
+  console.log(e.target.id);
+  // 메인페이지 - 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
+  if (e.target.id !== "userImg") {
+    $("#userImg").classList.remove("toggle");
+    $(".menubar").style.display = "none";
+  } // if
+
+  if (e.target.id !== "search") {
+    $(".cancel").style.display = "none";
+  }
+
+  // 이벤트 체크용
+  // console.log(e.target.value)
+  // console.log(e.target.innerText)
+});
+
+// 메인페이지 - 회원 이미지 클릭 시 메뉴바 표시
+$("#userImg").addEventListener("click", () => {
+  if ($("#userImg").classList.toggle("toggle")) {
+    $(".menubar").style.display = "block";
+  } else {
+    $(".menubar").style.display = "none";
+  }
+});
+
+$("#search").addEventListener("click", () => {
+  console.log("click");
+  $(".cancel").style.display = "block";
+});

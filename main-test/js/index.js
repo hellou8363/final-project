e6e557a6364f88fbe4daed5189aa73b9dafd1372
eunhye han <hellou8363@gmail.com>
2023-02-15 16:38:50 +0900

    통합검색바 입력창 포커스 시 취소(X) 아이콘 표시

--- main-test/js/index.js
+++ main-test/js/index.js
@@ -2,20 +2,11 @@ const $ = (selector) => document.querySelector(selector);
 
 // Event init
 addEventListener("click", (e) => {
-  console.log(e.target.id);
   // 메인페이지 - 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
   if (e.target.id !== "userImg") {
     $("#userImg").classList.remove("toggle");
     $(".menubar").style.display = "none";
   } // if
-
-  if (e.target.id !== "search") {
-    $(".cancel").style.display = "none";
-  }
-
-  // 이벤트 체크용
-  // console.log(e.target.value)
-  // console.log(e.target.innerText)
 });
 
 // 메인페이지 - 회원 이미지 클릭 시 메뉴바 표시
@@ -27,7 +18,19 @@ $("#userImg").addEventListener("click", () => {
   }
 });
 
-$("#search").addEventListener("click", () => {
-  console.log("click");
-  $(".cancel").style.display = "block";
+// 메인페이지 - 산정보, 모집, 후기 글 목록 위에서
+// 휠 스크롤 시 가로 슬라이드 구현
+$(".mountain-info .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY * 2;
+});
+
+$(".recruit .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY * 2;
+});
+
+$(".review .wrap").addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY * 2;
 });

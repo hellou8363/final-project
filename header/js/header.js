5a2471db844ca6d3aa91f62bde07250bbf9c89a9
eunhye han <hellou8363@gmail.com>
2023-02-16 11:08:30 +0900

    메인페이지 내 Rank에 미디어쿼리 적용

--- header/js/header.js
+++ header/js/header.js
@@ -1,24 +1,20 @@
 const $ = (selector) => document.querySelector(selector);
 
-// Event init
+// Header Event init
 addEventListener("click", (e) => {
-  console.log(e.target.id);
-  // 메인페이지 - 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
+  // 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
   if (e.target.id !== "userImg") {
     $("#userImg").classList.remove("toggle");
     $(".menubar").style.display = "none";
   } // if
 
+  // 검색바에 포커스가 없을 때 입력테스트 삭제버튼 숨기기
   if (e.target.id !== "search") {
     $(".cancel").style.display = "none";
-  }
-
-  // 이벤트 체크용
-  // console.log(e.target.value)
-  // console.log(e.target.innerText)
+  } // if
 });
 
-// 메인페이지 - 회원 이미지 클릭 시 메뉴바 표시
+// 회원 이미지 클릭 시 메뉴바 표시
 $("#userImg").addEventListener("click", () => {
   if ($("#userImg").classList.toggle("toggle")) {
     $(".menubar").style.display = "block";
@@ -27,6 +23,7 @@ $("#userImg").addEventListener("click", () => {
   }
 });
 
+// 검색바에 포커스가 있을 때 입력테스트 삭제버튼 표시
 $("#search").addEventListener("click", () => {
   console.log("click");
   $(".cancel").style.display = "block";

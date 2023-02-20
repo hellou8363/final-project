4adf66bd8bfe30d2401d54a03dfa795857f75c7b
eunhye han <hellou8363@gmail.com>
2023-02-20 11:49:35 +0900

    통합결과페이지 샘플 이미지 추가

--- search-result/js/header.js
+++ search-result/js/header.js
@@ -1,30 +1,30 @@
-const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelector(selector);
 
 // Header Event init
 addEventListener("click", (e) => {
   // 회원 메뉴바가 열린상태에서 다른 곳을 클릭하면 메뉴바 숨기기
   if (e.target.id !== "userImg") {
-    $("#userImg").classList.remove("toggle");
-    $(".menubar").style.display = "none";
+    $$("#userImg").classList.remove("toggle");
+    $$(".menubar").style.display = "none";
   } // if
 
   // 검색바에 포커스가 없을 때 입력테스트 삭제버튼 숨기기
   if (e.target.id !== "search") {
-    $(".cancel").style.display = "none";
+    $$(".cancel").style.display = "none";
   } // if
 });
 
 // 회원 이미지 클릭 시 메뉴바 표시
-$("#userImg").addEventListener("click", () => {
-  if ($("#userImg").classList.toggle("toggle")) {
-    $(".menubar").style.display = "block";
+$$("#userImg").addEventListener("click", () => {
+  if ($$("#userImg").classList.toggle("toggle")) {
+    $$(".menubar").style.display = "block";
   } else {
-    $(".menubar").style.display = "none";
+    $$(".menubar").style.display = "none";
   }
 });
 
 // 검색바에 포커스가 있을 때 입력테스트 삭제버튼 표시
-$("#search").addEventListener("click", () => {
+$$("#search").addEventListener("click", () => {
   console.log("click");
-  $(".cancel").style.display = "block";
+  $$(".cancel").style.display = "block";
 });
